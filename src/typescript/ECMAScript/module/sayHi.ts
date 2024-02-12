6e59ef4d4e2e316edb1e8c5c228e34fce56d392e
Springcomp <springcomp@users.noreply.github.com>
2024-02-12 17:45:45 +0100

    first ECMAScript module in TypeScript

---
+++ src/typescript/ECMAScript/module/sayHi.ts
@@ -0,0 +1,7 @@
+// 📁 sayHi.ts
+export function sayHi(user: string): void {
+  console.log(`Hello, ${user}!`);
+}
+export function sayGoodbye(user: string): void {
+  console.log(`Good bye, ${user}!`);
+}
