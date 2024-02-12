550ed00675153660290575b44fca623bb26c06c7
Springcomp <springcomp@users.noreply.github.com>
2024-02-12 17:45:47 +0100

    native UMD in TypeScript

---
+++ src/typescript/UMD/apps/CommonJS/index.js
@@ -0,0 +1,6 @@
+// 📁 main.js
+const { sayGoodbye, sayHi } = require('../../module/dist/sayHi.js');
+
+console.log(sayHi); // function...
+sayHi('John'); // Hello, John!
+sayGoodbye('John'); // Good bye, John!
